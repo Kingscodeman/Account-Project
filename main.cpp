2e22b9029a2a0b88335615e47f0ee50e48090412
Leo <107164619+Kingscodeman@users.noreply.github.com>
2025-06-06 16:23:27 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "stock.h"
-#include "account.h"
+#include "foreign_account.h"
 #include "person.h"
 using namespace std;
 
@@ -10,6 +10,8 @@ int main() {
     Peggy->deposit(100);
     Peggy->withdraw(10);
     Peggy->show_balance();
+
+    delete Peggy;
     cout<<"\n(2) Combination account operation (Stock & Basic account)\n";
 
     stock::add("NFLX");
@@ -18,68 +20,60 @@ int main() {
     stock::add("NVDA");
     stock::s_rank();
 
+    PersonAccount* Tom=new PersonAccount("Tom");
+    ForeignAccount::set_rate("USD", 32.0);
+    ForeignAccount::set_rate("JPY", 0.22);
 
-    PersonAccount *Tom=new PersonAccount("Tom");
-    ForeignAccount::set_rate("USD",32.0);
-    ForeignAccount::set_rate("JPY",0.22);
-    ForeignAccount* for_account = Tom->access_foreign_account("USD");
-
-    Tom->get_BasicAccount()->set_account_num(949000509949);
-    Tom->get_BasicAccount()->get_account_info();
+    ForeignAccount* for_account=Tom->access_foreign_account("USD");
+    Tom->set_account_num(949000509949);
+    Tom->get_account_info();
 
     for_account->set_account_num(233000509949);
-    for_account->get_account_info() ;
+    for_account->get_account_info();
 
     Tom->access_foreign_account("USD")->deposit(100);
     Tom->access_foreign_account("JPY")->deposit(100);
     Tom->access_foreign_account("USD")->withdraw(50);
 
-    Tom->get_BasicAccount()->deposit(100);
+    Tom->Account::deposit(100);
     Tom->transfer_to_stock(50);
-    Tom->get_BasicAccount()->show_balance();
-    Tom->get_StockAccount()->show_balance();
-
+    Tom->Account::show_balance();
+    Tom->stock::show_balance();
+    Tom->stock::deposit(300);
 
     // StockAccount deposit purchase
-    Tom->get_StockAccount()->deposit(300);
-    Tom->get_StockAccount()->purchase("DIS", 3000);
-    Tom->get_StockAccount()->purchase("NVDA", 2000);
-    Tom->get_StockAccount()->purchase("WMT", 1000);
-    Tom->get_StockAccount()->show_balance();
+    Tom->purchase("DIS",3000);
+    Tom->purchase("NVDA",2000);
+    Tom->purchase("WMT",1000);
+    Tom->stock::show_balance();
 
     // sell stock and check balance
-    Tom->get_StockAccount()->sell("DIS", 1000);
+    Tom->sell("DIS", 1000);
     Tom->show_all();
 
-    // renew stock info
     stock::change();
-
-    Tom->get_StockAccount()->yield("DIS");
+    Tom->yield("DIS");
     Tom->show_all();
     //ROI
-    Tom->get_StockAccount()->revenue();
+    Tom->revenue();
 
     delete Tom;
-    cout << "(3) Basic Account & Stock Account Operations with vector" << "\n";
 
-    vector<PersonAccount*> account_list;
+    cout<<"(3) Basic Account & Stock Account Operations with vector\n";
+    
+    vector<Account*> account_list;
+    account_list.push_back(new Account("Bob",false));
+    account_list.push_back(new stock("Charlie"));
 
-    account_list.push_back(new PersonAccount("Bob"));
-    account_list.push_back(new PersonAccount("Charlie"));
-
-    for (int i=0;i<account_list.size() ;i++) {
-        account_list[i]->get_BasicAccount()->deposit(200);
-        account_list[i]->get_BasicAccount()->withdraw(50);
-        account_list[i]->get_StockAccount()->deposit(300);
-        account_list[i]->get_StockAccount()->purchase("NVDA", 1000);
-        account_list[i]->show_all();
+    for (int i=0;i<account_list.size();i++){
+        account_list[i]->deposit(200);
+        account_list[i]->withdraw(50);
+        account_list[i]->show_balance();
     }
 
-
-    for (auto person:account_list) {
+    for (auto person : account_list){
         delete person;
     }
+
     return 0;
-
 }
-
